c6e4afca6f70809eef31a017dbd0489b72d92945
kumarapusrivarsha <134908628+kumarapusrivarsha@users.noreply.github.com>
2026-07-19 12:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+console.log("Code Optimization Ordered List Loaded");
+
+
+// Display number of optimization points
+
+let items = document.querySelectorAll("ol li");
+
+
+console.log("Total Optimization Techniques:", items.length);
